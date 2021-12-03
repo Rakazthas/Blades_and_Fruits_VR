b757e123a669c7292793b17d7292472b23f1a01d
Rakazthas <guillaume.lsn@gmail.com>
2021-12-03 11:02:12 +0100

    Bomb prefab + menu

--- Assets/Scripts/GlobalVars.cs
+++ Assets/Scripts/GlobalVars.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class GlobalVars : MonoBehaviour
 {
     public static float time = 0.0f;
-    public static float score = 0;
+    public static int score = 0;
     public static int lives = 3;
+
+    public static List<int> bestScore;
 
     public static bool inGame = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        bestScore = new List<int>(5);
+
+        for(int i = 0; i < 5; i++)
+        {
+            bestScore[i] = 0;
+        }
     }
 
     // Update is called once per frame
